3fd9b21bcfe7b3921f5d3a8200ec4d18a1fcb06a
Chatri Ngambenchawong <pingkunga@gmail.com>
2024-07-22 13:42:12 +0700

    feat: add sample page

---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <VContainer>
+    <VRow>
+      <VCol>
+         <VCard>
+            <VCardTitle>Card Title - หัวข้อหน้าหลัก</VCardTitle>
+            <VCardText>Card Text</VCardText>
+         </VCard>
+      </VCol>
+    </VRow>
+    <!-- Button -->
+    <VRow>
+      <VCol>
+        <VBtn color="primary">Primary</VBtn>
+        <VBtn color="secondary">Secondary</VBtn>
+        <VBtn color="success">Success</VBtn>
+        <VBtn color="danger">Danger</VBtn>
+        <VBtn color="warning">Warning</VBtn>
+        <VBtn color="info">Info</VBtn>
+        <VBtn color="light">Light</VBtn>
+        <VBtn color="dark">Dark</VBtn>
+        <VBtn color="link">Link</VBtn>
+      </VCol>
+    </VRow>
+   </VContainer>
+</template>
+
+<script setup lang="ts">
+import { VBtn } from 'vuetify/components';
+
+
+</script>
+
+<style scoped>
+
+</style>
