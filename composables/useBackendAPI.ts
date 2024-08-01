dd2d163b195d4c15767673cf09cfb32087acb870
Chatri Ngambenchawong <pingkunga@gmail.com>
2024-08-01 23:23:31 +0700

    feat: add backendapi

---
+++ composables/useBackendAPI.ts
@@ -0,0 +1,40 @@
+export default() => {
+
+    const router = useRouter()
+
+    const config = useRuntimeConfig()
+    const api = config.public.url
+
+    const token = useCookie('token')
+
+    const headers = {
+         'Accept': 'application/json',
+         'Content-Type': 'application/json',
+         'Authorization': `Bearer ${token.value}`
+    }
+ 
+    const fetchWithTokenCheck = async <T>(url: any, options: any) => {
+        const response = await useFetch(url, options)
+        if(response.error.value && (response.error.value.statusCode === 403 || response.error.value.statusCode === 401)) {
+            //Token expired redirect to login
+            router.push('/')
+        }
+
+        return response
+    }
+
+    const getAllProducts = async(page: number, limit: number) => {
+        return fetchWithTokenCheck<any>(
+            `${api}/products?page=${page}&limit=${limit}`,
+            {
+                method: 'GET',
+                headers: headers,
+                cache: 'no-cache'
+            }
+        )
+    }
+    
+    return {
+        getAllProducts
+    }
+}
